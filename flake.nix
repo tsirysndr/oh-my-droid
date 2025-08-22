a630ff401161f7196cfdfcf294158ea5f30c7ba1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-08-22 09:54:17 +0300

    bump version code

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           inherit src;
 
           pname = "oh-my-droid";
-          version = "0.1.0";
+          version = "0.2.0";
 
           buildInputs = [
             # Add additional build inputs here
